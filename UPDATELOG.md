b6c67a49ee21f43dad2c8ccb897234ddcb62c833
Bosn <Bosn@10.32.240.24>
2012-07-24 11:12:48 +0800

    * validation for JSON String in page testers √ 07/24/2012   * add more comment for my code √ 07/24/2012

--- UPDATELOG.md
+++ UPDATELOG.md
@@ -1,12 +1,16 @@
+======================rap v1.2 package=================================================
 `DONE`
+  * validation for JSON String in page testers √ 07/24/2012
+  * add more comment for my code √ 07/24/2012
   * add project introduction in README.md 07/18/2012
+  * add new path configuration, static path of local and remote can be same. 07/19/2012
 
 `TODO`
-  * validation for JSON String in page testers
-  * add more comment for my code
   * add local storage function prevent from data loss
   * change literal for some are too old
   * fix shortcuts compatibilities in different browsers (1.1 only support Firefox)
   * add project introduction in README.md √
+   * validation for JSON String in page testers √
+  * add more comment for my code √
 
 `BUG`
--- src/com/baidu/rigel/rap/project/bo/Action.java
+++ src/com/baidu/rigel/rap/project/bo/Action.java
@@ -9,61 +9,62 @@ import java.util.Set;
 
 import com.baidu.rigel.rap.common.StringUtils;
 
-public class Action implements java.io.Serializable{
+public class Action implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	private String name;
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	private String description;
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description == null ? "" : description;
 	}
-	
+
 	private String requestType = "1";
-	
+
 	public String getRequestType() {
 		return requestType;
 	}
-	
+
 	public void setRequestType(String requestType) {
-		if (requestType == null || requestType == "") return;
+		if (requestType == null || requestType == "")
+			return;
 		this.requestType = requestType;
 	}
-	
+
 	private String requestUrl;
-	
+
 	public String getRequestUrl() {
 		return requestUrl;
 	}
-	
+
 	public void setRequestUrl(String requestUrl) {
 		this.requestUrl = requestUrl == null ? "" : requestUrl;
 	}
-	
+
 	public void addParameter(Parameter parameter, boolean isRequest) {
 		if (isRequest) {
 			getRequestParameterList().add(parameter);
@@ -73,27 +74,27 @@ public class Action implements java.io.Serializable{
 			parameter.getActionResponseList().add(this);
 		}
 	}
-	
+
 	private Set<Parameter> requestParameterList = new HashSet<Parameter>();
-	
+
 	public Set<Parameter> getRequestParameterList() {
 		return requestParameterList;
 	}
-	
+
 	public void setRequestParameterList(Set<Parameter> requestParameterList) {
 		this.requestParameterList = requestParameterList;
 	}
-	
+
 	private Set<Parameter> responseParameterList = new HashSet<Parameter>();
-	
+
 	public Set<Parameter> getResponseParameterList() {
 		return responseParameterList;
 	}
-	
+
 	public void setResponseParameterList(Set<Parameter> responseParameterList) {
 		this.responseParameterList = responseParameterList;
 	}
-	
+
 	public List<Parameter> getRequestParameterListOrdered() {
 		Set<Parameter> parameterList = getRequestParameterList();
 		List<Parameter> parameterListOrdered = new ArrayList<Parameter>();
@@ -101,7 +102,7 @@ public class Action implements java.io.Serializable{
 		Collections.sort(parameterListOrdered, new ParameterComparator());
 		return parameterListOrdered;
 	}
-	
+
 	public List<Parameter> getResponseParameterListOrdered() {
 		Set<Parameter> parameterList = getResponseParameterList();
 		List<Parameter> parameterListOrdered = new ArrayList<Parameter>();
@@ -109,27 +110,28 @@ public class Action implements java.io.Serializable{
 		Collections.sort(parameterListOrdered, new ParameterComparator());
 		return parameterListOrdered;
 	}
-	
+
 	private String responseTemplate;
-	
+
 	public String getResponseTemplate() {
 		return responseTemplate;
 	}
-	
+
 	public void setResponseTemplate(String responseTemplate) {
-		this.responseTemplate = responseTemplate == null ? "" : responseTemplate;
+		this.responseTemplate = responseTemplate == null ? ""
+				: responseTemplate;
 	}
-	
+
 	private Set<Page> pageList = new HashSet<Page>();
-	
+
 	public Set<Page> getPageList() {
 		return pageList;
 	}
-	
+
 	public void setPageList(Set<Page> pageList) {
 		this.pageList = pageList;
 	}
-	
+
 	public void update(Action action) {
 		setDescription(action.getDescription());
 		setName(action.getName());
@@ -137,33 +139,38 @@ public class Action implements java.io.Serializable{
 		setRequestUrl(action.getRequestUrl());
 		setResponseTemplate(action.getResponseTemplate());
 	}
-	
+
 	public String toString() {
-		StringBuilder stringBuilder = new StringBuilder();		
-		
+		StringBuilder stringBuilder = new StringBuilder();
+
 		stringBuilder.append("{\"id\":" + getId() + ",");
-		stringBuilder.append("\"name\":\"" + StringUtils.escapeInJ(getName()) + "\",");
-		stringBuilder.append("\"description\":\"" + StringUtils.escapeInJ(getDescription()) + "\",");
+		stringBuilder.append("\"name\":\"" + StringUtils.escapeInJ(getName())
+				+ "\",");
+		stringBuilder.append("\"description\":\""
+				+ StringUtils.escapeInJ(getDescription()) + "\",");
 		stringBuilder.append("\"requestType\":\"" + getRequestType() + "\",");
-		stringBuilder.append("\"requestUrl\":\"" + StringUtils.escapeInJ(getRequestUrl()) + "\",");
-		stringBuilder.append("\"responseTemplate\":\"" + StringUtils.escapeInJ(getResponseTemplate()) + "\",");
+		stringBuilder.append("\"requestUrl\":\""
+				+ StringUtils.escapeInJ(getRequestUrl()) + "\",");
+		stringBuilder.append("\"responseTemplate\":\""
+				+ StringUtils.escapeInJ(getResponseTemplate()) + "\",");
 		stringBuilder.append("\"requestParameterList\":");
 
 		stringBuilder.append("[");
-		Iterator<Parameter> iterator = getRequestParameterListOrdered().iterator();
-		while(iterator.hasNext()) {
+		Iterator<Parameter> iterator = getRequestParameterListOrdered()
+				.iterator();
+		while (iterator.hasNext()) {
 			stringBuilder.append(iterator.next());
 			if (iterator.hasNext()) {
 				stringBuilder.append(",");
 			}
 		}
 		stringBuilder.append("],");
-		
+
 		stringBuilder.append("\"responseParameterList\":");
 
 		stringBuilder.append("[");
 		iterator = getResponseParameterListOrdered().iterator();
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			stringBuilder.append(iterator.next());
 			if (iterator.hasNext()) {
 				stringBuilder.append(",");
@@ -171,5 +178,15 @@ public class Action implements java.io.Serializable{
 		}
 		stringBuilder.append("]}");
 		return stringBuilder.toString();
+	}
+	
+	private String remarks;
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
 	}
 }
