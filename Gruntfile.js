deedff355599561c482675e4dd839ac6f277500d
Chen Yicai <yicai.cyj@taobao.com>
2013-11-25 20:37:30 +0800

    Added grunt task to update base; added jekyll config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,31 @@
 module.exports = function(grunt) {
 
-  var request = require('superagent')
+  var request = require('request')
   var unzip = require('unzip')
+  var fs = require('fs')
+
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json')
   })
 
   grunt.registerTask('update', function() {
-    request
-      .get('https://github.com/thx/base/archive/master.zip')
-      .pipe(unzip.Extract({ path: '.' }))
+    var done = this.async()
+
+    request('https://github.com/thx/base/archive/master.zip')
+      .pipe(unzip.Parse())
+      .on('entry', function(entry) {
+        var epath = entry.path
+        var fpath = epath.replace(/^[^\/]+\//, '')
+
+        if (/_includes|assets/.test(epath) && !/\/$/.test(epath)) {
+          entry.pipe(fs.createWriteStream(fpath))
+          grunt.log.writeln('Updated ' + fpath)
+        }
+        else {
+          grunt.log.writeln('Skipped ' + fpath)
+        }
+      })
+      .on('close', done)
   })
 }
