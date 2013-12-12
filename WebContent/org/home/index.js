26b51d594fdaf47c8acc0270908c5969f4caf465
wangjeaf <wangjeaf@gmail.com>
2013-12-12 16:49:01 +0800

    add basic fe logic

--- WebContent/org/home/index.js
+++ WebContent/org/home/index.js
@@ -1,3 +1,88 @@
 $(function() {
-	$(".content").html('home here');
+	//$(".content").html('group here');
+	
+	var groups = {
+		groups: [
+		{
+			id: 1,
+			name: '<i class="glyphicon glyphicon-user"></i> 我的项目',
+			projects: [
+			    {
+			    	id: 1,
+			    	name: '测试项目11',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    },
+			    {
+			    	id: 2,
+			    	name: '测试项目',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    },
+			    {
+			    	id: 3,
+			    	name: '测试项目',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    },
+			    {
+			    	id: 4,
+			    	name: '测试项目',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    }
+			]
+		},{
+			id: 1,
+			name: '<i class="glyphicon glyphicon-heart"></i> 我关注的项目',
+			projects: [
+			    {
+			    	id: 1,
+			    	name: '测试项目11',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    },
+			    {
+			    	id: 2,
+			    	name: '测试项目',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    }
+			]
+		},{
+			id: 1,
+			name: '<i class="glyphicon glyphicon-star"></i> 重要项目',
+			projects: [
+			    {
+			    	id: 1,
+			    	name: '测试项目11',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    },
+			    {
+			    	id: 2,
+			    	name: '测试项目',
+			    	desc: '这是一个测试项目',
+			    	status: '最近更新：2小时前'
+			    }
+			]
+		}]
+	};
+	
+	function bindEvents() {
+		$('body')
+		.delegate('.box', 'click', function() {
+			var box = $(this);
+			if (!box.hasClass('box')) {
+				box = box.parent('.box');
+			}
+			var projId = box.data('projid');
+			window.location.href="/rap/project.action?id=" + projId;
+		})
+	}
+	var tmpl = $('#group-tmpl').text();
+	
+	var html = $.render(tmpl, groups);
+	$(".groups").append(html);
+	bindEvents();
 });
--- WebContent/org/productline/index.js
+++ WebContent/org/productline/index.js
@@ -1,3 +1,119 @@
 $(function() {
-	$(".content").html('productline here');
+	function render() {
+		//$(".content").html('home here');
+		var data = {
+			items: [{
+				id: 1,
+				name: '测试产品线1',
+				count: 123
+			},
+			{
+				id: 2,
+				name: '测试产品线2',
+				count: 123
+			},
+			{
+				id: 3,
+				name: '测试产品线3',
+				count: 123
+			},
+			{
+				id: 4,
+				name: '测试产品线4',
+				count: 123
+			}]
+		};
+		
+		var tmpl = $('#table-rows').text();
+		$('#pl-items').html($.render(tmpl, data));
+	}
+	
+	function createProductline() {
+		$.confirm({
+			content: $.render($('#create-productline').text(), {}),
+			title: '添加产品线',
+			confirmText: '确定',
+			showCallback: function() {
+				$(this).find('input[type=text]').focus();
+			},
+			confirmClicked: function() {
+				var inputer = $(this).find('input[type=text]');
+				if (inputer.val().trim() == '') {
+					inputer.addClass('shake');
+					inputer.focus();
+					setTimeout(function() {
+						inputer && inputer.removeClass('shake');
+					}, 1000);
+					return;
+				}
+				var tmpl = $('#table-rows').text();
+				$('#pl-items').append($.render(tmpl, {items:
+					[{
+						id: 5,
+						name: $(this).find('input[type=text]').val(),
+						count: 0
+					}]
+				}
+				));
+				$(this).modal('hide');
+			}
+		});
+	}
+	
+	function updateProductline() {
+		var jqThis = $(this);
+		var id = jqThis.data('id');
+		var name = jqThis.parents('.pl-item').find('.name').text();
+		$.confirm({
+			content: $.render($('#create-productline').text(), {value: name}),
+			title: '更新产品线',
+			confirmText: '确定',
+			showCallback: function() {
+				$(this).find('input[type=text]').focus();
+			},
+			confirmClicked: function() {
+				var inputer = $(this).find('input[type=text]');
+				if (inputer.val().trim() == '') {
+					inputer.addClass('shake');
+					inputer.focus();
+					setTimeout(function() {
+						inputer && inputer.removeClass('shake');
+					}, 1000);
+					return;
+				}
+				$('.pl-' + id).find('.name').html($(this).find('input[type=text]').val());
+				$(this).modal('hide');
+			}
+		});
+	}
+	
+	function deleteProductline() {
+		var jqThis = $(this);
+		var id = jqThis.data('id');
+		var name = jqThis.parents('.pl-item').find('.name').text();
+		$.confirm({
+			content: '删除以后不能恢复，请谨慎操作',
+			title: '您确定要删除产品线 ' + name + ' 吗？',
+			confirmText: '确定删除',
+			confirmClicked: function() {
+				var node = $('.pl-' + id);
+				node.remove();
+				$(this).modal('hide');
+			}
+		});
+	}
+	
+	function bindEvents() {
+		$('body')
+			.delegate('.create-btn', 'click', createProductline)
+			.delegate('.update-btn', 'click', updateProductline)
+			.delegate('.delete-btn', 'click', deleteProductline);
+	}
+	
+	function init() {
+		render();
+		bindEvents();
+	}
+	
+	init();
 });
