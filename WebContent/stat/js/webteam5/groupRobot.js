d361e390d987868256eb4accc204e70203d6913b
Bosn Ma <huoyong.msb@alibaba-inc.com>
2013-12-27 17:37:40 +0800

    web team

---
+++ WebContent/stat/js/webteam5/groupRobot.js
@@ -0,0 +1,87 @@
+(function() {
+ var LaiWang = {
+   autoEventVoice: function() {
+     var selfId = '{{id}}';
+     var msgs = ['好吃，我喜欢', '流口水了', '真好看'];
+     var send = function(arr) {
+       console.log(arr.length);
+       var item = arr.shift();
+       if (item) {
+         $.ajax({
+           url: '/score/add/' + item.appId + '/' + item.id + '.json',
+           type: 'POST',
+           data: {
+             scoreType: Math.round(Math.random() * 5) + 1,
+             uidAppItemOwner: item.uid
+           },
+           complete: function() {
+             $.ajax({
+               url: '/comment/add/' + item.appId + '/' + item.id + '/' +
+item.uid + '.xhtml',
+               type: 'POST',
+               data: {
+                 content: msgs[Math.round(Math.random() * (msgs.length -
+1))]
+               },
+               complete: function() {
+                 setTimeout(function() {
+                   send(arr);
+                 }, 5000 + Math.random() * 5000);
+               }
+             });
+           }
+         });
+       }
+     };
+
+     var readList = function() {
+       var lis = document.getElementById('eventStreamWrapper').childNodes;
+       var a = [];
+       for (var i = lis.length - 1, li; i >= 0; i--) {
+         li = lis[i];
+         if (li && li.tagName && li.tagName == 'LI') {
+           var anchors = li.getElementsByTagName('a');
+           var sent = false;
+           for (var j = anchors.length - 1, anchor; j >= 0; j--) {
+             anchor = anchors[j];
+             if (anchor.getAttribute('data-uid') == selfId) {
+               sent = true;
+               break;
+             }
+           }
+           if (!sent) {
+             a.push({
+               uid: li.getAttribute('data-uid'),
+               id: li.getAttribute('data-id'),
+               appId: li.getAttribute('data-app-id')
+             });
+           }
+         }
+       }
+       send(a);
+     };
+     var loadMore = function(flag) {
+       var node = document.getElementById('eventStreamWrapper').parentNode;
+       var e = document.createEvent('MouseEvents');
+       e.initEvent('click', true, true);
+       var last = node.lastChild;
+       while (last.nodeType != 1) {
+         last = last.previousSibling;
+       }
+       last.dispatchEvent(e);
+       if (flag < 5) {
+         setTimeout(function() {
+           loadMore(flag + 1);
+         }, 5000);
+       } else {
+         readList();
+       }
+     };
+
+
+     loadMore(0);
+   }
+ };
+
+ LaiWang.autoEventVoice();
+})();
