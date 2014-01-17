ea7b6083a7bb67cc06d1fee13891109aac1ad2ce
Chen Yicai <yicai.cyj@taobao.com>
2014-01-17 14:40:07 +0800

    Updated base

--- assets/post.js
+++ assets/post.js
@@ -77,10 +77,10 @@ KISSY.use('brix/app,node', function(S, app, Node) {
             li.removeClass('folded')
         })
 
-        S.all('article').all('h1,h2,h3').each(function(heading) {
+        S.all('article').all('h2,h3,h4,h5,h6').each(function(heading) {
             var id = heading.attr('id')
             heading.prepend('<a name="' + id + '" class="anchor"></a>')
-            heading.prepend('<a name="' + id + '" class="anchor-link" href="#' + id + '">#</a>')
+            heading.prepend('<a class="anchor-link" href="#' + id + '"></a>')
         })
     }
 })
