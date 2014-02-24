3377b46f9a194cc34bbf994b9e8a6ef4e5f6e49c
Bosn Ma <huoyong.msb@alibaba-inc.com>
2014-02-24 16:41:39 +0800

    doc

---
+++ src/Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double v1 = 0.1;
+		double v2 = 0.2;
+		System.out.println(v1 + v2);
+			
+	}
+
+}
