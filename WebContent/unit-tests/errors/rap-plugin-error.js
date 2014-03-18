82412454172a43b21b4c7eb00c89fc1b7936ac86
wangjeaf <wangjeaf@gmail.com>
2014-03-18 16:10:50 +0800

    add S.config testcases

--- WebContent/unit-tests/errors/rap-plugin-error.js
+++ WebContent/unit-tests/errors/rap-plugin-error.js
@@ -1,9 +1,24 @@
 module('bugfix');
 
 test('without io: KISSY.use("node", function(S, A) {})', function() {
-	stop();
+	stop(4);
 	KISSY.use('node', function(S, Node) {
-		start();
+		start(1);
+		ok(S.config, 'still has S.config');
+	});
+	
+	KISSY.use('node, io', function(S, Node) {
+		start(1);
+		ok(S.config, 'still has S.config');
+	});
+	
+	KISSY.use('node, io', function(S, Node, IO) {
+		start(1);
+		ok(S.config, 'still has S.config');
+	});
+	
+	KISSY.use('node, io, node', function(S, Node, IO, Node) {
+		start(1);
 		ok(S.config, 'still has S.config');
 	});
 })
