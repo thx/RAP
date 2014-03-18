8cb4bc069fc8ed4e10dfd99e7238864dc0be8279
wangjeaf <wangjeaf@gmail.com>
2014-03-18 14:44:15 +0800

    add tests

--- WebContent/unit-tests/index.js
+++ WebContent/unit-tests/index.js
@@ -2,9 +2,12 @@ var root = /unit-tests/;
 
 async.map([
 	'getrule/base.js',
+	
 	'getruledata/base.js',
-	'rap-plugin/base.js'
+	
+	'rap-plugin/base.js',
+	'rap-plugin/jsapis.js'
 ], function(s, callback) {
-	$.getScript(root + s).done(callback);
+	$.getScript(root + s + '?' + (+new Date)).done(callback);
 }, function(err, results) {
 });
---
+++ WebContent/unit-tests/rap-plugin/jsapis.js
@@ -0,0 +1,17 @@
+module('rap-plugin-jsapis');
+
+test('RAP.set/getWhiteList', function() {
+	ok(true, 'ok');
+});
+
+test('RAP.set/getHost', function() {
+	ok(true, 'ok');
+});
+
+test('RAP.set/getBlackList', function() {
+	ok(true, 'ok');
+});
+
+test('RAP.set/getMode', function() {
+	ok(true, 'ok');
+})
