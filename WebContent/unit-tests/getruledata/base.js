940da8c319255b8817e755bb3e631da510da2ce2
wangjeaf <wangjeaf@gmail.com>
2014-03-19 11:52:07 +0800

    add unit tests

--- WebContent/unit-tests/getruledata/base.js
+++ WebContent/unit-tests/getruledata/base.js
@@ -1,6 +1,6 @@
 module('get-mockjs-rule-data');
 
-test('jQuery - getMockjsData - /mockjsdata/projectId/action', function() {
+test('jQuery - getMockjsData - get "/mockjsdata/projectId/action"', function() {
 	stop();
 	$.ajax({  
         type : "get",  
@@ -18,7 +18,7 @@ test('jQuery - getMockjsData - /mockjsdata/projectId/action', function() {
     });  
 });
 
-test('KISSY - getMockjsData - /mockjsdata/projectId/action', function() {
+test('KISSY - getMockjsData - get "/mockjsdata/projectId/action"', function() {
 	stop();
 	KISSY.oldUse('io', function(S, IO) {
 		IO({
@@ -35,5 +35,29 @@ test('KISSY - getMockjsData - /mockjsdata/projectId/action', function() {
 	        error:function(){  
 	        }  
 		})
+	});
+});
+
+test('KISSY - getMockjsData - setPrefix("/mockjsdata/") and just get by action', function() {
+	stop();
+	var base = '/mockjs/base';
+	KISSY.use('io', function(S, IO) {
+		var old = RAP.getPrefix();
+		RAP.setPrefix('/mockjsdata/');
+		IO({
+			type : "get",  
+	        url : base,  
+	        dataType : "jsonp",
+	        jsonp: "callback",
+	        success : function(data){
+	        	start();
+	        	ok('a' in data, 'property exists');
+	        	ok(KISSY.isNumber(data.a), 'a is number');
+	        	ok(data.a >= 1 && data.a <= 10, 'a between 1 and 10');
+	        },  
+	        error:function(){  
+	        }  
+		})
+		RAP.setPrefix(old);
 	});
 });
