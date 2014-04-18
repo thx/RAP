2724f8784f43485aaaaae45cffe5ea83116580a0
Bosn Ma <huoyong.msb@alibaba-inc.com>
2014-04-18 12:34:33 +0800

    parameter templatilize

--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,7 @@
-import com.taobao.rigel.rap.common.StringUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-
+import com.taobao.rigel.rap.common.Patterns;
 
 public class Tester {
 
@@ -8,10 +9,18 @@ public class Tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String val = "<script src=\"http://rap.alibaba-inc.com/rap.plugin.js?projectId=159&mode=0\"></script>";
-		
-		System.out.println(StringUtils.escapeInH(val));
-			
+		int num = 1;
+		Pattern p = Pattern.compile(Patterns.MOCK_TEMPLATE_PATTERN);
+		Matcher matcher = p.matcher("${page=1008}");
+		while (matcher.find()) {
+			System.out.println("找到的第" + num++ + "组");
+			int c = matcher.groupCount();
+			while (c-- > 0 ? true : false) {
+				System.out.println("[" + c + "] " + matcher.group(c));
+			}
+
+		}
+
 	}
 
 }
