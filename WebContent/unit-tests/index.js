2422bdec1f253c2de13d1025c03f8833257b4d7a
Bosn Ma <huoyong.msb@alibaba-inc.com>
2014-04-24 14:38:05 +0800

    dev

--- WebContent/unit-tests/index.js
+++ WebContent/unit-tests/index.js
@@ -1,23 +1,22 @@
 module('env confirm');
 test('no KISSY.io when kissy seed inited', function() {
-	ok(KISSY.io === undefined, 'KISSY.io is undefined when kissy seed loaded');
-})
-
+    ok(KISSY.io === undefined, 'KISSY.io is undefined when kissy seed loaded');
+});
 
 var scripts = [
-	'getrule/base.js',
-	
-	'getruledata/base.js',
-	'getruledata/datatypes.js',
-	
-	'rap-plugin/base.js',
-	'rap-plugin/jsapis.js',
-	'rap-plugin/modes.js',
-	'rap-plugin/whiteblack.js',
-	
-	'errors/rap-plugin-error.js'
+    'utility/structureValidator.js',
+
+    'getrule/base.js',
+
+    'getruledata/base.js',
+    'getruledata/datatypes.js',
+
+    'rap-plugin/base.js',
+    'rap-plugin/jsapis.js',
+    'rap-plugin/modes.js',
+    'rap-plugin/whiteblack.js',
+
+    'errors/rap-plugin-error.js'
 ];
 
-loadScripts(scripts, function() {
-	
-});
+loadScripts(scripts, function() {});
