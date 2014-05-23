d4817c462c0f965832ea726942b0876d16961d90
Bosn Ma <huoyong.msb@alibaba-inc.com>
2014-05-23 10:56:41 +0800

    dev

--- src/com/taobao/rigel/rap/organization/bo/Corporation.java
+++ src/com/taobao/rigel/rap/organization/bo/Corporation.java
@@ -2,8 +2,6 @@ package com.taobao.rigel.rap.organization.bo;
 
 public class Corporation {
 	private int id;
-	private String logoUrl;
-	private int userId;
 	private String name;
 
 	public int getId() {
@@ -12,22 +10,6 @@ public class Corporation {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public String getLogoUrl() {
-		return logoUrl;
-	}
-
-	public void setLogoUrl(String logoUrl) {
-		this.logoUrl = logoUrl;
-	}
-
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
 	}
 
 	public String getName() {
