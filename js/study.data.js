49832638ed24aad3c1a0ca8c7be98bc31752a424
霍雍 <huoyong.msb@alibaba-inc.com>
2014-10-11 14:50:46 +0800

    add links

--- js/study.data.js
+++ js/study.data.js
@@ -18,33 +18,41 @@
                     'url'   : 'http://cloud.video.taobao.com//play/u/1018757965/p/2/e/1/t/1/17661220.swf'
                 }, {
                     'title' : '创建项目',
-                    'url'   : 'http://cloud.video.taobao.com//play/u/1018757965/p/1/e/1/t/1/17661402.swf'
+                    'url'   : 'http://cloud.video.taobao.com//play/u/1018757965/p/2/e/1/t/1/17661402.swf'
                 }, {
-                    'title' : '管理组织结构'
+                    'title' : '管理组织结构',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17767958.swf'
                 }
             ]
         }, {
             'title' : '文档编辑',
             'contents' : [
                 {
-                    'title' : '文档编辑入门'
+                    'title' : '文档编辑入门',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17768437.swf'
                 }, {
-                    'title' : '版本控制'
+                    'title' : '版本控制',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17768638.swf'
                 }, {
-                    'title' : 'JSON导入'
+                    'title' : 'JSON导入',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17770570.swf'
                 }
             ]
         }, {
             'title' : 'MOCK工具',
             'contents' : [
                 {
-                    'title' : 'MOCK工具入门'
+                    'title' : 'MOCK工具入门',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17772031.swf'
                 }, {
-                    'title' : 'MOCK工具进阶'
+                    'title' : 'MOCK工具进阶',
+                    'url'   : ''
                 }, {
-                    'title' : '项目间共享MOCK数据'
+                    'title' : '项目间共享MOCK数据',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17775467.swf'
                 }, {
-                    'title' : 'RESTFul API的支持'
+                    'title' : 'RESTFul API的支持',
+                    'url'   : 'http://cloud.video.taobao.com//play/u/11051796/p/2/e/1/t/1/17774663.swf'
                 }
             ]
         }
